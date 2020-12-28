23969fc9386b540ec5d3165aa7ee92112a15c903
abhinavRai23 <raiabhinavrai1994@gmail.com>
2020-12-29 04:23:04 +0530

    react boilerplate initial setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+	const listData = [
+		'React17',
+		'Webpack5',
+		'SASS support',
+		'HMR',
+		'Seperate CSS bundle',
+		'Error Boundary'
+	];
+	return (
+		<div className="main">
+			<h1>You are ready to Rock</h1>
+		</div>
+	);
+}
+
+export default App;
